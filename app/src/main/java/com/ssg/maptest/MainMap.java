9b0b9c59c11af329bf4a140c16a04d31979fedb9
stevenfontanella <steven.fontanella@gmail.com>
2018-10-06 19:50:48 -0400

    Commit stuff

--- app/src/main/java/com/ssg/maptest/MainMap.java
+++ app/src/main/java/com/ssg/maptest/MainMap.java
@@ -1,6 +1,8 @@
 package com.ssg.maptest;
 
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -14,10 +16,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.maps.android.SphericalUtil
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainMap extends FragmentActivity implements OnMapReadyCallback {
+public class MainMap extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
 
     private GoogleMap lmap;
@@ -29,6 +33,7 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback {
     private static final int ACTIVE_COLOR = 0x66fcff6c;
     private static final int STROKE_WIDTH = 3;
     private double[] coordinates = new double[2];
+    HashMap<LatLng, Polygon> hexagons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,36 +53,37 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback {
         coordinates[1] = -77.86;
         Log.d("LAT", Double.toString(coordinates[0]));
         Log.d("LONG", Double.toString(coordinates[1]));
-        ArrayList<Polygon> hexagons = plotHexMesh(coordinates, 10);
+        hexagons = plotHexMesh(coordinates, 10);
     }
 
-    private ArrayList<Polygon> plotHexMesh(double[] city, int iterationnumber) {
-            int scalefactor = 1000;
-            ArrayList<Polygon> cityhexagons = new ArrayList<>();
-            double[] center;
-            Polygon centerhex = lmap.addPolygon(new PolygonOptions().clickable(true).fillColor(ACTIVE_COLOR).
-                    strokeWidth(STROKE_WIDTH).strokeColor(STROKE_COLOR).add (
-                    new LatLng(city[0] + elementlength * Math.pow(3, 0.5), city[1] + elementlength),
-                    new LatLng(city[0], city[1] + elementlength * 2),
-                    new LatLng(city[0] - elementlength * Math.pow(3, 0.5), city[1] + elementlength),
-                    new LatLng(city[0] - elementlength * Math.pow(3, 0.5), city[1] - elementlength),
-                    new LatLng(city[0], city[1] - elementlength * 2),
-                    new LatLng(city[0] + elementlength * Math.pow(3, 0.5), city[1] - elementlength)));
-            cityhexagons.add(centerhex);
-            for (double shell = 1; shell < iterationnumber; shell ++) {
-                for (int side = 0; side < 6; side ++) {
-                    for (double hexagon = 0; hexagon < shell - 1; hexagon ++) {
-                        center = findCenter(city, elementlength * Math.pow(3, 0.5) / 2, shell, side, hexagon);
-                        Polygon cityhex = lmap.addPolygon(new PolygonOptions().clickable(true).fillColor(FILL_COLOR).
-                                strokeWidth(STROKE_WIDTH).strokeColor(STROKE_COLOR).add(
-                                new LatLng(center[0] + elementlength * Math.pow(3, 0.5), center[1] + elementlength),
-                                new LatLng(center[0], center[1] + elementlength * 2),
-                                new LatLng(center[0] - elementlength * Math.pow(3, 0.5), center[1] + elementlength),
-                                new LatLng(center[0] - elementlength * Math.pow(3, 0.5), center[1] - elementlength),
-                                new LatLng(center[0], center[1] - elementlength * 2),
-                                new LatLng(center[0] + elementlength * Math.pow(3, 0.5), center[1] - elementlength)));
-                        cityhexagons.add(cityhex);
-                    }
+    private HashMap<LatLng, Polygon> plotHexMesh(double[] city, int iterationnumber) {
+//            Log.d("A","Ran plotHexMesh");
+        int scalefactor = 1000;
+        ArrayList<Polygon> cityhexagons = new ArrayList<>();
+        double[] center;
+        Polygon centerhex = lmap.addPolygon(new PolygonOptions().clickable(true).fillColor(ACTIVE_COLOR).
+                strokeWidth(STROKE_WIDTH).strokeColor(STROKE_COLOR).add (
+                new LatLng(city[0] + elementlength * Math.pow(3, 0.5), city[1] + elementlength),
+                new LatLng(city[0], city[1] + elementlength * 2),
+                new LatLng(city[0] - elementlength * Math.pow(3, 0.5), city[1] + elementlength),
+                new LatLng(city[0] - elementlength * Math.pow(3, 0.5), city[1] - elementlength),
+                new LatLng(city[0], city[1] - elementlength * 2),
+                new LatLng(city[0] + elementlength * Math.pow(3, 0.5), city[1] - elementlength)));
+        cityhexagons.add(centerhex);
+        for (double shell = 1; shell < iterationnumber; shell ++) {
+            for (int side = 0; side < 6; side ++) {
+                for (double hexagon = 0; hexagon < shell - 1; hexagon ++) {
+                    center = findCenter(city, elementlength * Math.pow(3, 0.5) / 2, shell, side, hexagon);
+                    Polygon cityhex = lmap.addPolygon(new PolygonOptions().clickable(true).fillColor(FILL_COLOR).
+                            strokeWidth(STROKE_WIDTH).strokeColor(STROKE_COLOR).add(
+                            new LatLng(center[0] + elementlength * Math.pow(3, 0.5), center[1] + elementlength),
+                            new LatLng(center[0], center[1] + elementlength * 2),
+                            new LatLng(center[0] - elementlength * Math.pow(3, 0.5), center[1] + elementlength),
+                            new LatLng(center[0] - elementlength * Math.pow(3, 0.5), center[1] - elementlength),
+                            new LatLng(center[0], center[1] - elementlength * 2),
+                            new LatLng(center[0] + elementlength * Math.pow(3, 0.5), center[1] - elementlength)));
+                    cityhexagons.add(cityhex);
+                }
             }
         }
         return cityhexagons;
@@ -113,5 +119,40 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback {
             center[1] = city[1] + (hexagon - shell) * elementlength * Math.pow(3, 0.5);
         }
         return center;
+    }
+
+    private void setCurrentPolyColored(LatLng latlng){
+        Polygon closest = null;
+        double min_dist_sq = Integer.MAX_VALUE;
+        for( LatLng l : hexagons.keySet()){
+//            double dist_sq = l;
+            double dist_sq = SphericalUtil.computeDistanceBetween(l, latlng);
+            if ( dist_sq < min_dist_sq){
+                min_dist_sq = dist_sq;
+                closest = hexagons.get(l);
+            }
+        }
+
+        closest.setFillColor(ACTIVE_COLOR);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        setCurrentPolyColored(new LatLng(location.getLatitude(), location.getLongitude()));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
